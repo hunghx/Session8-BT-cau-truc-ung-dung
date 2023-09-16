e036134d32aea93a9706e3aa6a1f03d418c02482
hunghx <baby10051999@gmail.com>
2023-09-16 21:48:06 +0700

    sesison8

--- src/ra/run/App.java
+++ src/ra/run/App.java
@@ -1,11 +1,50 @@
 package ra.run;
 
+import ra.model.Account;
 import ra.service.AccountService;
 
+import java.util.Scanner;
+
 public class App {
+    public static Scanner scanner = new Scanner(System.in);
     private static AccountService accountService = new AccountService();
     public static void main(String[] args) {
+        register();
+        // hiển thị menu
+        while(true){
+            // 1 login
+            // 2 register
+            // 3 thoát
 
+            // nhập lựa chọn
+            // switch case
+            // case 1: login()
+
+            // case 2 : register()
+        }
+
+
+    }
+    public static void login(){
+        // hiển thị nhập thông tin
+        // kiểm tra thông tin hợp lệ hay không
+        // nếu hợp lệ thì hiện thị ra trang home (chào mưng đến với trang chủ)
+    }
+    public static void register(){
+        Account account = new Account();
+        // hiển thị form nhập thông tin
+        // kiểm tra thông tin đã xác thực chưa
+        // nếu đã xác thưc thì hiển thị ra trang đăng nhập (login())
+        System.out.println("Nhập username");
+        while(true){
+            String username = new Scanner(System.in).nextLine();
+            if(accountService.existByUsername(username)){
+                System.out.println("Đã tồn tại, vui lòng nhập tên khác");
+            }else {
+                account.setUsername(username);
+                break;
+            }
+        }
 
     }
 }
--- src/ra/service/AccountService.java
+++ src/ra/service/AccountService.java
@@ -4,8 +4,30 @@ import ra.model.Account;
 
 public class AccountService {
     private Account[] accounts = new Account[100];
-    // đăng nhập, đăng kí
-    public void login(){
 
+    public AccountService() {
+        accounts[0] = new Account("hunghx","hung@gmail.com","123456");
+    }
+
+    // đăng nhập, đăng kí
+    public boolean login(String username , String password){
+        for (int i = 0; i < accounts.length; i++) {
+            if (accounts[i] != null
+                    && accounts[i].getUsername().equals(username)
+                    &&accounts[i].getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean existByUsername(String username){
+        for (int i = 0; i < accounts.length; i++) {
+            if (accounts[i] != null
+                    && accounts[i].getUsername().equals(username)
+            ) {
+                return true;
+            }
+        }
+        return false;
     }
 }
